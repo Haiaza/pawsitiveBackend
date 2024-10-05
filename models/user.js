8452076fa7ad979225d88df0adfc3dcbed372c31
haiaza <ajames1342@gmail.com>
2024-10-05 14:13:04 -0400

    Removed hashed PW from Signup res

--- models/user.js
+++ models/user.js
@@ -25,5 +25,11 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        delete returnedObject.hashedPassword;
+    }
+})
+
 const User = mongoose.model('User', userSchema)
 module.exports = User
