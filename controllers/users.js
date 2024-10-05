e45b971de6cb8f2bda4bfadc250fc07116863427
haiaza <ajames1342@gmail.com>
2024-10-05 14:07:00 -0400

    User Controller SignIn and SignUp are working

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,62 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
 
-router.get('/sign-token', (req, res) => {
-  res.json({ message: 'You are authorized!' });
+const SALT_LENGTH = 12
+
+router.post('/signup', async (req, res) => {
+    try {
+      // Check if the username is already taken
+        const userInDatabase = await User.findOne({ username: req.body.username });
+        if (userInDatabase) {
+        return res.json({ error: 'Username already taken.' });
+    }
+      // Create a new user with hashed password
+        const user = await User.create({
+        username: req.body.username,
+        hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH),
+        email: req.body.email
+    });
+        const token = jwt.sign(
+        { username: user.username, _id: user._id },
+        process.env.JWT_SECRET
+    );
+        res.status(201).json({ user, token });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+})
+
+router.post('/signin', async (req, res) => {
+    try {
+        // Hey DB Find this user
+        const user = await User.findOne({ username: req.body.username });
+
+        /// If we find a user, compare the password they supplied to the one in the DB
+        if (user && bcrypt.compareSync(req.body.password, user.hashedPassword)) {
+            
+        
+            //SUCCESS!!
+
+            //Create a signed token now!
+            const token = jwt.sign(
+                
+            { username: user.username, _id: user._id },
+            process.env.JWT_SECRET
+        );
+
+        //
+        res.status(200).json({ token });
+    } else {
+        res.status(401).json({ error: 'Invalid username or password.' });
+    }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
+
+
 
 module.exports = router;
