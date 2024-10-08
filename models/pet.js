80aaa17463fb8c50441912fcd7c6073406a5c1b6
haiaza <ajames1342@gmail.com>
2024-10-08 00:21:24 -0400

    character casing correction

--- models/pet.js
+++ models/pet.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const petSchema = new mongoose.Schema({
     name:           String,
     breed:          String,
-    Age:            Number,
+    age:            Number,
     isAdopted: {
         type:       Boolean,
         default:    false
